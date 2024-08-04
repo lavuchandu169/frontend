bebb8676820c90caf5239b9b7d8e5e1a7227caf5
lavuchandu169 <82090789+lavuchandu169@users.noreply.github.com>
2024-08-04 14:40:58 +0100

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,7 +8,7 @@ const LandingPage = () => {
   const loginUser = useSelector((state) => state.login.loginUser);
   const dispatch = useDispatch();
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/verifyUser", {
+    fetch("https://backend-7ksj.onrender.com/verifyUser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
