a96564b8838c82f46eeb7f664a4ed2a46340d761
lavuchandu169 <82090789+lavuchandu169@users.noreply.github.com>
2024-08-04 14:07:00 +0100

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -68,7 +68,7 @@ const Register = () => {
       return;
     }
     try {
-      const response = await fetch("http://127.0.0.1:5000/register", {
+      const response = await fetch("https://backend-7ksj.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
